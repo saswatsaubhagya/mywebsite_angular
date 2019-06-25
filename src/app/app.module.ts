e21ff64ed0bd4562434b1ae561eb5eb089bf6c7d
Saswat Saubhagya Rout <saswat.srout@sparcindia.com>
2019-06-25 17:54:08 +0530

    dock added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavigatorComponent } from './navigator/navigator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
